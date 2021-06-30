b46d8bf5e792c0891de67d7c48bc56ec0b9405c8
williampenguin <70587024+williampenguin@users.noreply.github.com>
2021-06-30 12:28:12 -0400

    Update read_in.py

--- WillianPan/read_in.py
+++ WillianPan/read_in.py
@@ -1,7 +1,7 @@
 def pocket_analysis(filename):
-    #read in
+    # read in
     data = open(filename)
-    #count number of pockets
+    # count number of pockets
     num_case = 1
 
     data_list = []
@@ -10,18 +10,18 @@ def pocket_analysis(filename):
         data_list.append(str(line))
     num_pocket = int(num_case/21)
 
-    #outer loop with number of pockets
+    # outer loop with number of pockets
     res = []
     for i in range(num_pocket):
         row = []
         for j in range(1, 20):
-        #split with : delimiter
+            # split with : delimiter
             info = data_list[i * 21 + j]
             temp = info.split(":")
             item = temp[1]
-            #take out /t /n
-            item = item[item.find('\t')+1:item.find('\n')]
+            # take out /t /n
+            item = item[item.find('\t') + 1 : item.find('\n')]
             row.append(float(item))
         res.append(row)
-    #return the list
+    # return the list
     return res
